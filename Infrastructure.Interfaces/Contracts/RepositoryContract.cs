b4e1f277461fb63f37dfd254632354a54ffc53e1
ashmind <ashmind@gmail.com>
2011-03-10 17:54:01 +0300

    Added contract to IRepository.

---
+++ Infrastructure.Interfaces/Contracts/RepositoryContract.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics.Contracts;
+
+namespace AshMind.LightWiki.Infrastructure.Interfaces.Contracts {
+    [ContractClassFor(typeof(IRepository<>))]
+    internal abstract class RepositoryContract<T> : IRepository<T> {
+        public T Load(string key) {
+            Contract.Requires<ArgumentException>(!string.IsNullOrWhiteSpace(key));
+            throw new NotSupportedException();
+        }
+
+        public void Save(T entity) {
+            Contract.Requires<ArgumentNullException>(entity != null);
+            throw new NotSupportedException();
+        }
+
+        public IEnumerable<T> Query() {
+            Contract.Ensures(Contract.Result<IEnumerable<T>>() != null);
+            throw new NotSupportedException();
+        }
+    }
+}
--- Infrastructure.Interfaces/IRepository.cs
+++ Infrastructure.Interfaces/IRepository.cs
@@ -1,8 +1,12 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.Contracts;
 using System.Linq;
 
+using AshMind.LightWiki.Infrastructure.Interfaces.Contracts;
+
 namespace AshMind.LightWiki.Infrastructure.Interfaces {
+    [ContractClass(typeof(RepositoryContract<>))]
     public interface IRepository<T> {
         T Load(string key);
         void Save(T entity);
